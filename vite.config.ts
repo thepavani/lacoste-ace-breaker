5729052842d4c49df4174112991c22daf155b69a
Pavani Ramagaini <pavaniramagaini@gmail.com>
2026-09-15 14:54:49 +0530

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/lacoste-ace-breaker/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
